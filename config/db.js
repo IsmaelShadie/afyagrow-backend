a96494acfd3d893b48a06c7a6c2e23f068cddc22
IsmaelShadie <belyibnsidiiq@gmail.com>
2026-04-26 18:21:35 +0300

    Disable SSL for Railway internal network

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 const { Pool } = require("pg");
 
+const connectionString = process.env.DATABASE_URL;
+const isExternal = connectionString && connectionString.includes("proxy.rlwy.net");
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  connectionString,
+  ssl: isExternal ? { rejectUnauthorized: false } : false,
 });
 
-pool.on("connect", () => console.log("DB connected successfully"));
+pool.on("connect", () => console.log("DB connected!"));
 pool.on("error", (err) => console.error("DB error:", err));
 module.exports = pool;
