af80029b5101a9dd68a236335f663207beec221e
IsmaelShadie <belyibnsidiiq@gmail.com>
2026-04-26 19:08:30 +0300

    Connect to Supabase

--- config/db.js
+++ config/db.js
@@ -1,13 +1,8 @@
 const { Pool } = require("pg");
-
-const connectionString = process.env.DATABASE_URL;
-const isExternal = connectionString && connectionString.includes("proxy.rlwy.net");
-
 const pool = new Pool({
-  connectionString,
-  ssl: isExternal ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
-
 pool.on("connect", () => console.log("DB connected!"));
 pool.on("error", (err) => console.error("DB error:", err));
 module.exports = pool;
