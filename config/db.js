27c2c77ae9e95746065d2e549b624832609992ce
IsmaelShadie <belyibnsidiiq@gmail.com>
2026-04-26 15:07:38 +0300

    Use internal Railway DB URL

--- config/db.js
+++ config/db.js
@@ -1,12 +1,8 @@
 const { Pool } = require("pg");
-
-const isProduction = process.env.NODE_ENV === "production";
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false, checkServerIdentity: () => undefined } : false,
+  ssl: false,
 });
-
 pool.on("connect", () => console.log("DB connected"));
 pool.on("error", (err) => console.error("DB error:", err));
 module.exports = pool;
